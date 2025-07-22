9950d70b11bbda5d912182b1c3523bbf91f41cfe
lizhibao <63389341+lzb-libra@users.noreply.github.com>
2025-07-22 15:53:54 +0800

    init project

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,10 @@ import Pet from '../views/Pet.vue'
 import About from '../views/About.vue'
 
 const routes = [
-  { path: '/', redirect: '/hero' },
-  { path: '/hero', name: 'Hero', component: Hero },
-  { path: '/pet', name: 'Pet', component: Pet },
-  { path: '/about', name: 'About', component: About },
+  { path: '/endless-winter/', redirect: '/hero' },
+  { path: '/endless-winter/hero', name: 'Hero', component: Hero },
+  { path: '/endless-winter/pet', name: 'Pet', component: Pet },
+  { path: '/endless-winter/about', name: 'About', component: About },
 ]
 
 const router = createRouter({
